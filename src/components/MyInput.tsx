77d2ea4f0aa4e6f4a4d88b09342555d35575185f
Piotr Proc <piotr.proc@gmail.com>
2025-04-06 17:06:06 +0200

    React Query hello world

--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import useCounter from "../hooks/useCounter";
+import useComments from "../hooks/useComments";
 
 function MyInput() {
 
     const counter = useCounter((state:any) => state.counter);
     const incrCounter = useCounter((state:any) => state.incrCounter);
+
+
+    const { data, error, isLoading } = useComments();
+    console.log(data);
 
     return (
         <>
