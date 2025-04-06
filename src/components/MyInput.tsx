21b0e7c5837c7034b15356329866a80b888a0ce8
Piotr Proc <piotr.proc@gmail.com>
2025-04-06 17:53:38 +0200

    React Query hello world 2

--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -1,21 +1,31 @@
+import {TextField} from "@material-ui/core";
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import React from 'react';
 import useCounter from "../hooks/useCounter";
 import useAutocompleteOptions from "../hooks/useAutocompleteOptions";
+import top100Films from "../films";
 
 function MyInput() {
 
     const counter = useCounter((state:any) => state.counter);
     const incrCounter = useCounter((state:any) => state.incrCounter);
 
+    const { data:options, error, isLoading } = useAutocompleteOptions("asd");
+    console.log(options);
 
-    const { data, error, isLoading } = useAutocompleteOptions("asd");
-    console.log(data);
+    if(!options) return <></>;
 
     return (
         <>
             <div><span>Hello</span> {counter}</div>
             <button onClick={incrCounter}>Increment</button>
             <input type="text"/>
+            <Autocomplete
+                getOptionLabel={(option) => option.word || ""}
+                disablePortal
+                options={options}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+            />
         </>
     );
 }
